4088c03734a5ce25adf4c831d871528a2c594a6f
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-07-09 19:08:18 -0300

    frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,27 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastify } from 'fastify';
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
+} from "fastify-type-provider-zod";
 // import { sql } from './db/connection.ts';
-import { env } from './env.ts';
-import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: "http://localhost:5173",
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.get('/health', () => {
-  return 'OK';
+app.get("/health", () => {
+  return "OK";
 });
 
 app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT });
-
-// 1:04:08
