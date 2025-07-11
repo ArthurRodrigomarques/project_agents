9f913c9104e393fb21c9efda8b36c56ce6ac8651
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-07-10 21:23:55 -0300

    feat

--- frontend/web/src/pages/create-room.tsx
+++ frontend/web/src/pages/create-room.tsx
@@ -1,36 +1,61 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
+// import { useQuery } from "@tanstack/react-query";
+// import { Link } from "react-router-dom";
 
-type GetRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+// type GetRoomsAPIResponse = Array<{
+//   id: string;
+//   name: string;
+// }>;
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-rooms"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3333/rooms");
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ["get-rooms"],
+  //   queryFn: async () => {
+  //     const response = await fetch("http://localhost:3333/rooms");
 
-      const result: GetRoomsAPIResponse = await response.json();
+  //     const result: GetRoomsAPIResponse = await response.json();
 
-      return result;
-    },
-  });
+  //     return result;
+  //   },
+  // });
 
   return (
-    <div>
-      {isLoading && <p>Carregando...</p>}
+    <div className="min-h-screen py-8 px-4">
+      {/* {isLoading && <p>Carregando...</p>} */}
 
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
+      <div className="mx-auto max-w-4xl">
+        <div className="grid gap-8 grid-cols-2 items-start">
+          <div />
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Salas Recentes</CardTitle>
+              <CardDescription>
+                Acesso rápido para as salas criadas recentemente
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+
+            </CardContent>
+          </Card>
+        </div>
+        {/* {data?.map((room) => {
           return (
             <Link key={room.id} to={`/room/${room.id}`}>
               {room.name}
             </Link>
           );
-        })}
+        })} */}
       </div>
     </div>
   );
 }
+
+
